4897b741ab61b6c8082abed8ad7bfd7f181fd434
RushaanRajeshPai <pairushaan64@gmail.com>
2025-02-08 23:35:34 +0530

    getting started with postman

--- src/app.js
+++ src/app.js
@@ -2,24 +2,22 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
-const app = express()
+const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
-}))
+}));
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
+// Routes
+import userRouter from './routes/user.routes.js';
 
-//routes
-import userRouter from './routes/user.routes.js'
+// Register routes
+app.use("/api/v1/users", userRouter);  // Correctly registering the user routes
 
-//routes declaration
-app.use("/api/v1/users", userRouter)  //url eg: http://localhost:8000/users/register
-
-
-export {app}
+export { app };
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,10 @@
 import { asyncHandler } from "../utils/asyncHandler";
 
-const registerUser = asyncHandler( async (req,res) => {
+const registerUser = asyncHandler(async (req, res) => {
+    console.log("Received POST request to /register");  // Debugging log
     return res.status(200).json({
         message: "OK"
-    })
-})
+    });
+});
 
-export {
-    registerUser,
-}
+export { registerUser };
