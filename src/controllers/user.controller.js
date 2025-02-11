d88dca6e420f8a92bcdcae0fe836ed0fbb01f56a
RushaanRajeshPai <pairushaan64@gmail.com>
2025-02-12 04:21:26 +0530

    validation and checking for same user done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,18 +1,36 @@
 import { asyncHandler } from "../utils/asyncHandler";
+import {ApiError} from "../utils/ApiError.js"
+import {User} from '../models/user.model.js'
 
 const registerUser = asyncHandler(async (req, res) => {
-    // Get user details from frontend (check user.model.js)
-    // Validation (email is in correct form, all boxes are not empty, etc)
-    // Check if user already exists (check if email/username is unique)
-    // Check for images, check for avatar
-    // Upload them on cloudinary
-    // Create user object - create entry in db
-    // Remove password and refresh token field from response (for security purpose)
-    // Check for user creation (if user is created succesfully)
-    // Return res i.e return response
+    // 1. Get user details from frontend (check user.model.js)
+    // 2. Validation (email is in correct form, all boxes are not empty, etc)
+    // 3. Check if user already exists (check if email/username is unique)
+    // 4. Check for images, check for avatar
+    // 5. Upload them on cloudinary
+    // 6. Create user object - create entry in db
+    // 7. Remove password and refresh token field from response (for security purpose)
+    // 8. Check for user creation (if user is created succesfully)
+    // 9. Return res i.e return response
 
+    // 1.
     const {fullName, email, userName, password} = req.body
     console.log("email: ", email)
+
+    // 2.
+    if(
+        [fullName,email,password,userName].some((field) => field?.trim() === "")
+    ){
+        throw new ApiError(400, "All field are required")
+    }
+
+    // 3.
+    const existedUser = User.findOne({
+        $or: [{userName}, {email}]
+    })
+    if(existedUser){
+        throw new ApiError(409,"User with same username or email exists")
+    }
 });
 
 export { registerUser };
