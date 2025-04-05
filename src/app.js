93f9393361ca5a5b78ebad5084e7bb7c61bd510e
RushaanRajeshPai <pairushaan64@gmail.com>
2025-04-05 14:37:24 +0530

    unsaved chgs

--- src/app.js
+++ src/app.js
@@ -23,6 +23,6 @@ app.get("/", (req, res) => {
 import userRouter from './routes/user.routes.js'
 
 // Register routes
-app.use("/api/v1/users", userRouter)  // Correctly registering the user routes
+app.use("/users", userRouter)  // Correctly registering the user routes
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import express from 'express'
 const app = express()
 
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 connectDB()
@@ -19,26 +19,3 @@ connectDB()
 .catch((err)=> {
     console.log("Mongodb connection failed", err);
 })
-
-
-/*
-import express from 'express'
-const app = express()
-(async() => {
-    try{
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.log("Errrorrr", error);
-            throw err
-        })
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listeneing on port ${process.env.PORT}`)
-        })
-    }
-    
-    catch(error){
-        console.log("Error : ", error)
-        throw err
-    }
-})()
-    */
